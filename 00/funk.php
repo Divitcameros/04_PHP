1fcc179f5459833cc4d71895c14e360518791e3d
Divit <hanma.oioioi@gmail.com>
2024-02-02 17:06:53 +0900

    funk.php DateTime test

--- 00/funk.php
+++ 00/funk.php
@@ -23,6 +23,7 @@ echo round(1.234567, 5);
 
 echo '<br>';
 
+// ランダム関数
 $colors = ['赤', '青', '緑', '黄色', '紫', '黒'];
 $index  = mt_rand(0, count($colors) - 1);
 echo '今日のラッキーカラーは' . $colors[$index] . 'です！<br>';
@@ -37,6 +38,7 @@ echo '今日のラッキーカラーは' . $colors[$index] . 'です！<br>';
 //   fx($aaa);
 // }
 
+// ユーザー定義関数
 function callNum()
 {
   return '080-XXXX-XXXX';
@@ -45,6 +47,7 @@ echo callNum() . '<br>';
 $phone = callNum();
 echo $phone . '<br>';
 
+// 引数の初期値
 function profile($name = '太郎', $age = 20, $gender = '男')
 {
   return 'こんにちは、' . $name . 'さん!
@@ -52,6 +55,7 @@ function profile($name = '太郎', $age = 20, $gender = '男')
 }
 echo profile(gender: '女', name: 'Jane', age: 24,); // 順番を気にせず引数を指定
 
+// 関数と配列
 $users = [
   'name'   => '太郎',
   'age'    => 24,
@@ -64,17 +68,18 @@ function fx($users)
 }
 echo fx($users);
 
-
+// アーリーリターン
 function isLeapYear($year) {
   if (empty($year)) return null;
-  return $year % 400 === 0 ? '閏年です' : 
-	($year % 100 === 0 ? '閏年ではありません' : 
-	($year % 4   === 0 ? '閏年です' : 
-	'閏年ではありません'));
+  return $year % 400 === 0 ? '閏年です'. '<br>' : 
+	($year % 100 === 0 ? '閏年ではありません' . '<br>': 
+	($year % 4   === 0 ? '閏年です' . '<br>': 
+	'閏年ではありません'. '<br>'));
 }
 echo isLeapYear(10);
 
 
+// Doccomment
 /**
  * Undocumented function
  *
@@ -86,14 +91,56 @@ function hasUserId(int $user_id): bool
   /* 判定処理 */
   return true;
 }
-hasUserId
-?>
 
 
-<html lang="ja">
-  <head>
-  </head>
-  <body>
-    
-  </body>
-</html>
+//****************************************
+//  タイムスタンプ
+//****************************************
+// 関数
+echo time() . '<br>';
+
+// クラス
+$d = new DateTime();
+echo $d->getTimestamp(). '<br>';
+
+
+//****************************************
+//  タイムスタンプ比較
+//****************************************
+$d1 = new DateTime('2024-03-03');
+$d2 = new DateTime('2024-02-27');
+
+$ts1 = $d1->getTimestamp();
+$ts2 = $d2->getTimestamp();
+
+// 比較
+if ($ts1 > $ts2) {
+    echo $d1->format('Y-m-d') . 'の方が新しいです。' . '<br>';
+} elseif ($ts1 < $ts2) {
+    echo $d2->format('Y-m-d') . 'の方が新しいです。' . '<br>';
+} else {
+    echo '同じ日付です'. '<br>';
+}
+
+//****************************************
+//  日数の差分比較
+//****************************************
+// 差分
+$interval = $d1->diff($d2);
+
+// 新旧判定の数値を取得(1なら新0なら旧)
+$invert = $interval->invert;
+
+// 日数の差分を取得
+$days = $interval->days;
+
+// 比較
+if ($days == 0) {
+    echo '日付は同じです。'. '<br>';
+} else {
+  if ($invert == 1) {
+      echo 'd1のほうが「' . $days . '日」新しいです'. '<br>';
+    } else {
+      echo 'd2のほうが「' . $days . '日」新しいです'. '<br>';
+  }
+}
