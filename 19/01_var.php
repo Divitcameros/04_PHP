4ab43ca751bf07f9f2bbf5c314827a62ba0ee3fb
Divit <hanma.oioioi@gmail.com>
2024-03-07 13:46:34 +0900

    01_var.php => 変数の代入と出力の練習

---
+++ 19/01_var.php
@@ -0,0 +1,21 @@
+<?php
+
+// ①
+$greeting = 'おはよう';
+echo $greeting . '<br>';
+
+// ②
+$greeting = 'こんにちは';
+echo $greeting . '<br>';
+
+// ③
+const DAYS_IN_WEEK = 7;
+echo DAYS_IN_WEEK . '<br>';
+
+// ④
+$x = '好きな果物は';
+$y = 'りんごです';
+$x .= $y;
+echo $x;
+
+?>
